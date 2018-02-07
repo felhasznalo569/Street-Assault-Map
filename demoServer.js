8bc4cbbf2983db410c73b6b12904748cdcbf1570
felhasznalo569 <32467707+felhasznalo569@users.noreply.github.com>
2018-02-07 22:48:42 +0100

    Delete demoServer.js

--- demoServer.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('demoMap.html', function(err, data) {
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
